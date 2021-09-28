1e5094d313b1977391740346789a8603455dfef7
Gabriel Guerin <gjguerin96@gmail.com>
2021-09-27 23:56:18 -0700

    Worked on javascript generatepassword function

--- script.js
+++ script.js
@@ -16,19 +16,36 @@ generateBtn.addEventListener("click", writePassword);
 
 var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numberArray = ["0","1","2","3","4","5","6","7","8","9"]
+var numbersArray = ["0","1","2","3","4","5","6","7","8","9"]
 var specCharArray = ["!","@","#","$","%","&","*","_","-","=","+","?","~"]
 
+
+
 function generatePassword() {
+  var passLength = prompt("Please enter desired password length, 8-24 characters.");
   var lowerCase = confirm("Do you want lowercase letters?")
-    if (lowerCase===true){}
   var upperCase = confirm("Do you want uppercase letters?")
-    if (upperCase===true){}
   var numbers = confirm("Do you want numbers?")
-    if (numbers===true){}
   var specChar = confirm("Do you want special characters?")
-    if (specChar===true){}
+  if (lowerCase===true && upperCase===true && numbers===true && specChar===true){
+    var availChar = lowerCaseArray.concat(upperCaseArray,numbersArray,specCharArray)
+    for (var i = 0; i < passLength; i++) {
+      var ranChar = Math.floor(Math.random() * availChar.length);
+      var comChoice = availChar[ranChar];
+      console.log(comChoice);
+    }
+  }
 }
 
-console.log(upperCaseArray);
+var numbers = [0, 1, 2, 3];
+numbersToString = numbers.join("")
+console.log(numbersToString);
+
+//function randomize(){
+  //var ranChar = Math.floor(Math.random() * availChar.length);
+  //var comChoice = availChar[ranChar];
+  //console.log(comChoice);
+//}
+
+
 generatePassword();
